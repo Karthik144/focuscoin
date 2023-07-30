c53145806dee2e614ba02e7b101c58a5efcaf9b6
“Karthik144” <karthik.k.ramu@gmail.com>
2023-07-30 15:36:07 +0530

    Fixed fonts and removed uneccessary comments

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import { Handjet } from '@next/font/google'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+
+
