315c1e1e058939481e913c103c6ae90d96950620
“Karthik144” <karthik.k.ramu@gmail.com>
2023-07-30 13:58:52 +0530

    Added export default statements to each page

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,11 +10,11 @@ const Navbar = () => {
         <Image className="nav-logo" src={logoImage} alt="Logo"/>
         <h1 className="nav-title">focuscoin</h1>
       </div>
-      <nav>
+      {/* <nav>
         <Link href="/login">Login</Link>
         <Link href="/signup">Signup</Link>
         <Link href="/wallet">Wallet</Link>
-      </nav>
+      </nav> */}
     </header>
   );
 };
