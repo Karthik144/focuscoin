ee96162c90da71dac1f4e2d9e1e17b3147e97e57
“Karthik144” <karthik.k.ramu@gmail.com>
2023-07-30 16:30:35 +0530

    Added whitepaper

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,6 +10,9 @@ const Navbar = () => {
         <Image className="nav-logo" src={logoImage} alt="Logo"/>
         <h1 className="nav-title">focuscoin</h1>
       </div>
+      <nav>
+        <a href="https://purrfect-verbena-aa2.notion.site/Focuscoin-Whitepaper-79d176f0b40e4f838f2886607902f715" target="_blank" rel="noopener noreferrer">Whitepaper</a>
+      </nav>
       {/* <nav>
         <Link href="/login">Login</Link>
         <Link href="/signup">Signup</Link>
